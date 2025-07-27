62952890f7ded08295223db5a71677fd8b2d0977
William Wong <compulim@hotmail.com>
2025-07-26 18:10:31 -0700

    Add content

--- packages/pages/src/app/App.tsx
+++ packages/pages/src/app/App.tsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useReduceMemo } from 'use-reduce-memo';
 
 const App = () => {
-  return false};
+  let numCalled = 0;
+
+  const result = useReduceMemo(
+    [1, 2, 3],
+    (sum, value) => {
+      numCalled++;
+
+      return sum + value;
+    },
+    0
+  );
+
+  return (
+    <Fragment>
+      <p>
+        Summation of <code>[1, 2, 3]</code> is {result}.
+      </p>
+      <p>Number of times called: {numCalled}</p>
+    </Fragment>
+  );
+};
 
 export default App;
